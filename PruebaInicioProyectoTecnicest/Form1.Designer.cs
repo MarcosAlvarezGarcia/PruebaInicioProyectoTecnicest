28f434b63331c298c073ff03eed877920fa9ef43
MarcosAlvarezGarcia <marcos.alv.garcia@gmail.com>
2024-03-20 13:32:59 +0100

    Set speed and coordinates list

--- PruebaInicioProyectoTecnicest/Form1.Designer.cs
+++ PruebaInicioProyectoTecnicest/Form1.Designer.cs
@@ -29,13 +29,19 @@
         private void InitializeComponent()
         {
             this.bttnStart = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.axisSelector = new System.Windows.Forms.ComboBox();
             this.labelAxis = new System.Windows.Forms.Label();
             this.bttnMin = new System.Windows.Forms.Button();
             this.bttnMax = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.axisCoordinate = new System.Windows.Forms.TextBox();
             this.regiserCoordinates = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.speedBar = new System.Windows.Forms.TrackBar();
+            this.coordinates = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.coordListBox = new System.Windows.Forms.ListBox();
+            this.searchedCoordsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).BeginInit();
             this.SuspendLayout();
             // 
             // bttnStart
@@ -43,7 +49,7 @@
             this.bttnStart.BackColor = System.Drawing.Color.Red;
             this.bttnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.bttnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.bttnStart.Location = new System.Drawing.Point(320, 94);
+            this.bttnStart.Location = new System.Drawing.Point(323, 11);
             this.bttnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.bttnStart.Name = "bttnStart";
             this.bttnStart.Size = new System.Drawing.Size(188, 105);
@@ -52,31 +58,20 @@
             this.bttnStart.UseVisualStyleBackColor = false;
             this.bttnStart.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label1.Location = new System.Drawing.Point(131, 23);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 48);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Controller";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
             // axisSelector
             // 
             this.axisSelector.Enabled = false;
+            this.axisSelector.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.axisSelector.FormattingEnabled = true;
             this.axisSelector.Items.AddRange(new object[] {
             "Eje X",
             "Eje Y",
             "Eje Z",
             "Eje U"});
-            this.axisSelector.Location = new System.Drawing.Point(16, 218);
+            this.axisSelector.Location = new System.Drawing.Point(19, 142);
             this.axisSelector.Margin = new System.Windows.Forms.Padding(4);
             this.axisSelector.Name = "axisSelector";
-            this.axisSelector.Size = new System.Drawing.Size(489, 24);
+            this.axisSelector.Size = new System.Drawing.Size(489, 28);
             this.axisSelector.TabIndex = 2;
             this.axisSelector.Text = "Select an axis";
             this.axisSelector.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -85,7 +80,7 @@
             // 
             this.labelAxis.AutoSize = true;
             this.labelAxis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.labelAxis.Location = new System.Drawing.Point(259, 273);
+            this.labelAxis.Location = new System.Drawing.Point(262, 205);
             this.labelAxis.Name = "labelAxis";
             this.labelAxis.Size = new System.Drawing.Size(155, 25);
             this.labelAxis.TabIndex = 3;
@@ -95,7 +90,7 @@
             // 
             this.bttnMin.Enabled = false;
             this.bttnMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.bttnMin.Location = new System.Drawing.Point(16, 262);
+            this.bttnMin.Location = new System.Drawing.Point(19, 194);
             this.bttnMin.Name = "bttnMin";
             this.bttnMin.Size = new System.Drawing.Size(114, 47);
             this.bttnMin.TabIndex = 4;
@@ -108,7 +103,7 @@
             // 
             this.bttnMax.Enabled = false;
             this.bttnMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.bttnMax.Location = new System.Drawing.Point(139, 262);
+            this.bttnMax.Location = new System.Drawing.Point(142, 194);
             this.bttnMax.Name = "bttnMax";
             this.bttnMax.Size = new System.Drawing.Size(114, 47);
             this.bttnMax.TabIndex = 5;
@@ -117,19 +112,19 @@
             this.bttnMax.MouseDown += new System.Windows.Forms.MouseEventHandler(this.bttnMax_MouseDown);
             this.bttnMax.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bttnMax_MouseUp);
             // 
-            // textBox1
+            // axisCoordinate
             // 
-            this.textBox1.Location = new System.Drawing.Point(420, 273);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(85, 22);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.Visible = false;
+            this.axisCoordinate.Location = new System.Drawing.Point(423, 205);
+            this.axisCoordinate.Name = "axisCoordinate";
+            this.axisCoordinate.Size = new System.Drawing.Size(85, 22);
+            this.axisCoordinate.TabIndex = 7;
+            this.axisCoordinate.Visible = false;
             // 
             // regiserCoordinates
             // 
             this.regiserCoordinates.Enabled = false;
             this.regiserCoordinates.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.regiserCoordinates.Location = new System.Drawing.Point(16, 437);
+            this.regiserCoordinates.Location = new System.Drawing.Point(19, 485);
             this.regiserCoordinates.Name = "regiserCoordinates";
             this.regiserCoordinates.Size = new System.Drawing.Size(492, 47);
             this.regiserCoordinates.TabIndex = 8;
@@ -137,23 +132,100 @@
             this.regiserCoordinates.UseVisualStyleBackColor = true;
             this.regiserCoordinates.Click += new System.EventHandler(this.regiserCoordinates_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(25, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Movement speed";
+            // 
+            // speedBar
+            // 
+            this.speedBar.Enabled = false;
+            this.speedBar.Location = new System.Drawing.Point(29, 300);
+            this.speedBar.Maximum = 100;
+            this.speedBar.Minimum = 1;
+            this.speedBar.Name = "speedBar";
+            this.speedBar.Size = new System.Drawing.Size(479, 56);
+            this.speedBar.TabIndex = 12;
+            this.speedBar.Value = 25;
+            this.speedBar.ValueChanged += new System.EventHandler(this.speedBar_ValueChanged);
+            // 
+            // coordinates
+            // 
+            this.coordinates.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.coordinates.AutoSize = true;
+            this.coordinates.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.coordinates.Location = new System.Drawing.Point(24, 435);
+            this.coordinates.Name = "coordinates";
+            this.coordinates.Size = new System.Drawing.Size(291, 25);
+            this.coordinates.TabIndex = 13;
+            this.coordinates.Text = "Coordinates: X (); Y (); Z (); U ()";
+            this.coordinates.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(25, 577);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Coordinates List";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // coordListBox
+            // 
+            this.coordListBox.FormattingEnabled = true;
+            this.coordListBox.ItemHeight = 16;
+            this.coordListBox.Location = new System.Drawing.Point(19, 623);
+            this.coordListBox.Name = "coordListBox";
+            this.coordListBox.Size = new System.Drawing.Size(489, 132);
+            this.coordListBox.TabIndex = 15;
+            this.coordListBox.SelectedIndexChanged += new System.EventHandler(this.coordListBox_SelectedIndexChanged);
+            // 
+            // searchedCoordsLabel
+            // 
+            this.searchedCoordsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchedCoordsLabel.AutoSize = true;
+            this.searchedCoordsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.searchedCoordsLabel.Location = new System.Drawing.Point(25, 769);
+            this.searchedCoordsLabel.Name = "searchedCoordsLabel";
+            this.searchedCoordsLabel.Size = new System.Drawing.Size(376, 25);
+            this.searchedCoordsLabel.TabIndex = 16;
+            this.searchedCoordsLabel.Text = "Searched coordinates: X (); Y (); Z (); U ()";
+            this.searchedCoordsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(523, 846);
+            this.Controls.Add(this.searchedCoordsLabel);
+            this.Controls.Add(this.coordListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.coordinates);
+            this.Controls.Add(this.speedBar);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.regiserCoordinates);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.axisCoordinate);
             this.Controls.Add(this.bttnMax);
             this.Controls.Add(this.bttnMin);
             this.Controls.Add(this.labelAxis);
             this.Controls.Add(this.axisSelector);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.bttnStart);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.speedBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,13 +234,18 @@
         #endregion
 
         private System.Windows.Forms.Button bttnStart;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox axisSelector;
         private System.Windows.Forms.Label labelAxis;
         private System.Windows.Forms.Button bttnMin;
         private System.Windows.Forms.Button bttnMax;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox axisCoordinate;
         private System.Windows.Forms.Button regiserCoordinates;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar speedBar;
+        private System.Windows.Forms.Label coordinates;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox coordListBox;
+        private System.Windows.Forms.Label searchedCoordsLabel;
     }
 }
 
--- PruebaInicioProyectoTecnicest/Form1.cs
+++ PruebaInicioProyectoTecnicest/Form1.cs
@@ -3,47 +3,58 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PruebaInicioProyectoTecnicest
 {
+
+
     public partial class Form1 : Form
     {
         private double axisX = 0;
         private double axisY = 0;
         private double axisZ = 0;
         private double axisU = 0;
-        private double valor = 0;
         private Timer timerAdd;
         private Timer timerSubstract;
+        public const int speedConstant = 5000;
+        public int speed;
+
 
         class Coordinates
         {
-            // Propiedades de las coordenadas x, y, z, u
+            // Propiedades de las coordenadas: name, x, y, z, u
+            public string name {  get; set; }
             public double X { get; set; }
             public double Y { get; set; }
             public double Z { get; set; }
             public double U { get; set; }
 
             // Constructor para inicializar las coordenadas
-            public Coordinates(double x, double y, double z, double u)
+            public Coordinates(string name, double X, double Y, double Z, double U)
             {
-                X = x;
-                Y = y;
-                Z = z;
-                U = u;
+                this.name = name;
+                this.X = X;
+                this.Y = Y;
+                this.Z = Z;
+                this.U = U;
             }
 
             // Método para imprimir las coordenadas del punto
             public void ImprimirCoordenadas()
             {
-                Console.WriteLine($"Coordenadas: ({X}, {Y}, {Z}, {U})");
-                MessageBox.Show($"Coordenadas: ({X}, {Y}, {Z}, {U})");
+                Console.WriteLine($"Coordinates: X({X}); Y({Y}); Z({Z}); U({U})");
+                MessageBox.Show($"Coordinates: X({X}); Y({Y}); Z({Z}); U({U})");
             }
         }
+
+        List<Coordinates> coordinatesList = new List<Coordinates>();
 
         public Form1()
         {
@@ -51,58 +62,70 @@ namespace PruebaInicioProyectoTecnicest
 
             //Crear un temporizador
             timerAdd = new Timer();
-            timerAdd.Interval = 1000; // Intervalo de tiempo en milisegundos
+            speed = speedConstant / speedBar.Value;
+            timerAdd.Interval = speed; // Intervalo de tiempo en milisegundos
             timerAdd.Tick += Timer_Tick_Add;
 
             timerSubstract = new Timer();
-            timerSubstract.Interval = 1000;
+            timerSubstract.Interval = speed;
             timerSubstract.Tick += Timer_Tick_Substract;
+
+            // Defino la propiedad de visualización en el ListBox del objeto Coordinate
+            coordListBox.DisplayMember = "name";
         }
 
         private void Timer_Tick_Add(object sender, EventArgs e)
         {
-            // Incrementar el valor y actualizar el texto del TextBox
+            // Incrementar el valor y actualizar los textos
             switch (axisSelector.SelectedIndex)
             {
                 case 0:
                     axisX++;
-                    textBox1.Text = Convert.ToString(axisX);
+                    axisCoordinate.Text = Convert.ToString(axisX);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
                 case 1:
                     axisY++;
-                    textBox1.Text = Convert.ToString(axisY);
+                    axisCoordinate.Text = Convert.ToString(axisY);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
                 case 2:
                     axisZ++;
-                    textBox1.Text = Convert.ToString(axisZ);
+                    axisCoordinate.Text = Convert.ToString(axisZ);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
                 case 3:
                     axisU++;
-                    textBox1.Text = Convert.ToString(axisU);
+                    axisCoordinate.Text = Convert.ToString(axisU);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
             }
         }
 
         private void Timer_Tick_Substract(object sender, EventArgs e)
         {
-            // Incrementar el valor y actualizar el texto del TextBox
+            // Disminuir el valor y actualizar el texto del TextBox
             switch (axisSelector.SelectedIndex)
             {
                 case 0:
                     axisX--;
-                    textBox1.Text = Convert.ToString(axisX);
+                    axisCoordinate.Text = Convert.ToString(axisX);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
                 case 1:
                     axisY--;
-                    textBox1.Text = Convert.ToString(axisY);
+                    axisCoordinate.Text = Convert.ToString(axisY);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
                 case 2:
                     axisZ--;
-                    textBox1.Text = Convert.ToString(axisZ);
+                    axisCoordinate.Text = Convert.ToString(axisZ);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
                 case 3:
                     axisU--;
-                    textBox1.Text = Convert.ToString(axisU);
+                    axisCoordinate.Text = Convert.ToString(axisU);
+                    coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z (" + axisZ + "); U (" + axisU + ")";
                     break;
             }
         }
@@ -116,6 +139,8 @@ namespace PruebaInicioProyectoTecnicest
                 bttnStart.BackColor = Color.Green;
                 axisSelector.Enabled = true;
                 regiserCoordinates.Enabled = true;
+                speedBar.Enabled = true;
+                coordinates.Text = "Coordinates: X (" + axisX + "); Y (" + axisY + "); Z ("+ axisZ + "); U (" + axisU + ")";
             }
             else
             {
@@ -126,8 +151,14 @@ namespace PruebaInicioProyectoTecnicest
                 bttnMax.Enabled = false;
                 axisSelector.Text = "Select an axis";
                 labelAxis.Text = "No axis selected";
-                textBox1.Visible = false;
+                axisCoordinate.Visible = false;
                 regiserCoordinates.Enabled = false;
+                speedBar.Value = 25;
+                speedBar.Enabled = false;
+                coordListBox.Enabled = false;
+                coordListBox.SelectedIndex = -1;
+                coordinates.Text = "Coordinates: X (); Y (); Z (); U ()";
+                searchedCoordsLabel.Text = "Searched coordinates: X (); Y (); Z (); U ()";
             }
         }
 
@@ -140,22 +171,22 @@ namespace PruebaInicioProyectoTecnicest
         {
             String axisSelected = axisSelector.SelectedItem as String;
             labelAxis.Text = axisSelected;
-            textBox1.Visible = true;
+            axisCoordinate.Visible = true;
             bttnMin.Enabled = true;
             bttnMax.Enabled = true;
             switch (axisSelector.SelectedIndex)
             {
                 case 0:
-                    textBox1.Text = Convert.ToString(axisX);
+                    axisCoordinate.Text = Convert.ToString(axisX);
                     break;
                 case 1:
-                    textBox1.Text = Convert.ToString(axisY);
+                    axisCoordinate.Text = Convert.ToString(axisY);
                     break;
                 case 2:
-                    textBox1.Text = Convert.ToString(axisZ);
+                    axisCoordinate.Text = Convert.ToString(axisZ);
                     break;
                 case 3:
-                    textBox1.Text = Convert.ToString(axisU);
+                    axisCoordinate.Text = Convert.ToString(axisU);
                     break;
             }
 
@@ -181,15 +212,88 @@ namespace PruebaInicioProyectoTecnicest
             timerAdd.Stop();
         }
 
+        private void speedBar_ValueChanged(object sender, EventArgs e)
+        {
+            speed = speedConstant / speedBar.Value;
+            // Actualizo el valor numérico en un TextBox
+            timerSubstract.Interval = speed;
+            timerAdd.Interval = speed;
+        }
+
         private void regiserCoordinates_Click(object sender, EventArgs e)
         {
+            string name = InputBox.Show("Enter a name for the coordinates", "Name entry");
             // Guardar un objeto de tipo Coordinates con coordenadas específicas
-            Coordinates coordinates = new Coordinates(axisX, axisY, axisZ, axisU);
+            Coordinates coordinates = new Coordinates(name, axisX, axisY, axisZ, axisU);
+            coordinatesList.Add(coordinates);
 
             // Imprimir las coordenadas del punto
             coordinates.ImprimirCoordenadas();
 
             Console.ReadLine();
+
+  
+            // Asigno la lista de objetos al DataSource de la lista
+            coordListBox.DataSource = null;
+            coordListBox.DataSource = coordinatesList;
+            // Defino la propiedad de visualización en el ListBox del objeto Coordinate
+            coordListBox.DisplayMember = "name";
+            coordListBox.SelectedIndex = -1;
+        }
+
+        private void coordListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Acciones que quieres realizar cuando se selecciona un elemento en el ListBox
+            if (coordListBox.SelectedIndex != -1) // Verificar si hay un elemento seleccionado
+            {            
+                Coordinates searchedCoordinates = coordListBox.SelectedItem as Coordinates;
+                searchedCoordsLabel.Text = "Searched coordinates: X (" + searchedCoordinates.X + "); Y (" + searchedCoordinates.Y + "); Z (" + searchedCoordinates.Z + "); U (" + searchedCoordinates.U + ")";
+            }
+        }
+    }
+
+    public static class InputBox
+    {
+        public static string Show(string prompt, string title)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = prompt;
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new System.Drawing.Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new System.Drawing.Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+                return textBox.Text;
+            else
+                return null;
         }
     }
 }
